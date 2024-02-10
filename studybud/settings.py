ced8d3310a35b4d047ae2b8302617b28fc21cb77
kiiru4reals <smaina991@gmail.com>
2024-02-10 11:52:11 +0300

    Added 0.0.0.0 and localhost to allowed hosts

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
